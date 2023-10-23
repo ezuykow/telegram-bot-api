a74ee82060cdc5020edf485b2281124f62bbeb9e
ezuykow <zuy1996@gmail.com>
2023-10-23 08:41:33 +0300

    init project

---
+++ src/main/java/kz/pandev/legrambotapi/LegramBotApiApplication.java
@@ -0,0 +1,13 @@
+package kz.pandev.legrambotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegramBotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegramBotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/pandev/legrambotapi/LegramBotApiApplicationTests.java
@@ -0,0 +1,14 @@
+package kz.pandev.legrambotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegramBotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//Проверка загрузки контекста
+	}
+
+}
